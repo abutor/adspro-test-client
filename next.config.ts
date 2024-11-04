3c01bff00ce4fef34289c7c9b0a971b68f68f42f
Aynur <latypov@aynur.dev>
2024-11-04 10:10:56 +0500

    fix build 2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@mdxeditor/editor'],
+  reactStrictMode: true,
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config
+  }
+
 };
 
 export default nextConfig;
